590c0c16f7cf6d3b20cc2b6054b30ffeeffaceff
stanlysandy9 <stanlysandy9@gmail.com>
2026-06-01 09:57:24 +0530

    New changes

--- tests/getApi6.spec.ts
+++ tests/getApi6.spec.ts
@@ -15,3 +15,16 @@ expect(reqRes.ok()).toBeTruthy();
 
 
 })
+test('get API verification with query parameters 2',async({request})=>{
+
+const reqRes = await request.get("/booking", {
+    params:{
+        firstname: "Joshw",
+        lastname: "Allenq"
+            }})
+console.log(await reqRes.json())
+expect(reqRes.status()).not.toBe(200);
+expect(reqRes.ok()).not.toBeTruthy();
+
+
+})
